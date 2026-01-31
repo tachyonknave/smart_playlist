c6c7a1c2794c3e70c5ecda4621a9eb204a398dd3
Kevin H <git@thehollywoods.net>
2026-01-30 23:38:02 -0500

    Credential Clean-up
    Throw away old credentials on failed refresh
    Update credential flow to get a new token when a refresh fails
    Fix credential check, null check when checking for refresh

--- src/credentials.py
+++ src/credentials.py
@@ -6,35 +6,42 @@ from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.exceptions import RefreshError
 
+logger = logging.getLogger(__name__)
+
+
 def should_get_new_token(credentials):
     should = False
 
     if not credentials:
+        logger.debug("No credentials found. Should get a new token")
         should = True
     elif not credentials.valid:
+        logger.debug("Credentials found but not valid. Should get a new token")
         should = True
 
     return should
 
+
 def get_new_token(secrets_file):
 
-    logging.info("Fetching new token...")
+    logger.info("Fetching new token...")
+    logger.debug(f"Loading secrets from file: {secrets_file}")
     flow = InstalledAppFlow.from_client_secrets_file(
-        secrets_file,
-        scopes=['https://www.googleapis.com/auth/youtube']
+        secrets_file, scopes=["https://www.googleapis.com/auth/youtube"]
     )
 
-    flow.run_local_server(port=8080, prompt='consent')
+    logger.debug("Running local server to get credentials...")
+    flow.run_local_server(port=8080, prompt="consent")
 
     return flow.credentials
+
 
 def creds_can_be_refreshed(credentials):
     should_be_refreshed = False
 
-    # TODO the refresh token may not work because it is too old
     # TODO use expiry instead of expired?
-
-    if credentials.expired and credentials.refresh_token:
+    if credentials and credentials.expired and credentials.refresh_token:
+        logger.debug("Token should be refreshed")
         should_be_refreshed = True
 
     return should_be_refreshed
@@ -42,37 +49,43 @@ def creds_can_be_refreshed(credentials):
 
 def refresh_credentials(credentials):
 
+    logger.info("Refreshing token...")
+    try:
+        credentials.refresh(Request())
+    except RefreshError as re:
+        logging.error("Refresh Error")
+        credentials = None
+
     return credentials
 
 
-def get_credentials(pickle_file='token.pickle', client_secrets_file='YtSmartPlaylist_client_secrets.json'):
+def get_credentials(
+    pickle_file="token.pickle",
+    client_secrets_file="YtSmartPlaylist_client_secrets.json",
+):
 
     credentials = None
 
+    # Try loading cached credentials from pickle file
     if os.path.exists(pickle_file):
-        logging.info("Loading credentials from file...")
-        with open(pickle_file, 'rb') as token:
+        logger.info(f"Loading credentials from file {pickle_file}...")
+        with open(pickle_file, "rb") as token:
             credentials = pickle.load(token)
 
+    # If token is expired but has a refresh token, attempt a refresh
+    if creds_can_be_refreshed(credentials):
+        credentials = refresh_credentials(credentials)
+
+    # If still no valid credentials (no pickle, refresh failed, etc.), do full re-auth
     if should_get_new_token(credentials):
+        credentials = get_new_token(client_secrets_file)
 
-        # Refresh or get new token
-        if should_get_new_token(credentials):
-            if creds_can_be_refreshed(credentials):
-                logging.info("Refreshing token...")
-                try:
-                    credentials.refresh(Request())
-                except RefreshError as re:
-                    logging.error("Refresh Error")
-            else:
-                credentials = get_new_token(client_secrets_file)
-
-        if credentials and credentials.valid:
-            logging.debug(f"Token expiration: {credentials.expiry}")
-            logging.info("Saving new token...")
-            with open(pickle_file, 'wb') as token:
-                logging.info("Saving credentials...")
-                pickle.dump(credentials, token)
+    # Persist valid credentials to pickle for next run
+    if credentials and credentials.valid:
+        logger.debug(f"Token expiration: {credentials.expiry}")
+        logger.info("Saving new token...")
+        with open(pickle_file, "wb") as token:
+            logger.info("Saving credentials...")
+            pickle.dump(credentials, token)
 
     return credentials
-
